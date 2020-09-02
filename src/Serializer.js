c19a372c9781021a44f4c22103b84871c285c838
Sceat <sceat@hydre.io>
2020-09-02 15:08:19 +0200

    the serializer now return some kind of basic ast

--- src/Serializer.js
+++ src/Serializer.js
@@ -1,52 +1,99 @@
 import { SYMBOLS } from './constant.js'
+import { inspect } from 'util'
 
-const { TRANSIENT, OPERATOR } = SYMBOLS
+/*
+ * The serializer take as input any javascript type
+ * and output an object which include
+ * - an array of keys as cypher parameters: foo=1 bar=2
+ * - a raw string of a query semgent: "SET n.name=$foo"
+ */
+
+const { OPERATOR } = SYMBOLS
+const unsuported_nested = object =>
+  new Error(`${ inspect(object, { depth: Infinity }) } \
+is a nested object and thus \
+not currently supported in redisgraph`)
 const Serializer = {
   // Ensure that redis graph understand the value we provide (https://oss.redislabs.com/redisgraph/result_structure/)
   // eslint-disable-next-line complexity, consistent-return
-  value: value => {
-    if (value === TRANSIENT) return ''
-    if (value === undefined || value === null) return 'NULL'
+  value: (value, prefix) => {
+    const with_value = x => ({
+      keys: [`${ prefix }=${ x }`],
+      raw : `$${ prefix }`,
+    })
+
+    if (value === undefined || value === null) return with_value('NULL')
+
     switch (typeof value) {
       case 'boolean':
-        return value ? 'true' : 'false'
+        return with_value(value ? 'true' : 'false')
 
       case 'bigint':
-        if (value > 9223372036854775807n) return `${ value }`
-        return value
+        if (value > 9223372036854775807n) return with_value(`${ value }`)
+        return with_value(value)
 
       case 'number':
-        if (value % 1) return Number.parseFloat(value).toPrecision(15)
-        return value
+        if (value % 1)
+          return with_value(Number.parseFloat(value).toPrecision(15))
+
+        return with_value(value)
 
       case 'string':
-        return `'${ value.replace('\'', '\\$&') }'`
+        return with_value(`'${ value.replace('\'', '\\$&') }'`)
 
       // eslint-disable-next-line no-use-before-define
       case 'object':
-        return Serializer.object(value)
+        return Serializer.object(value, prefix)
 
       case 'function':
 
       case 'symbol':
-        return 'NULL'
+        return with_value('NULL')
 
       // no default
     }
   },
-  object: object => {
-    if (Array.isArray(object)) return `[${ object.map(Serializer.value) }]`
+  object: (object, prefix) => {
+    if (Array.isArray(object)) {
+      if (object.some(x => x && typeof x === 'object' && !Array.isArray(x)))
+        throw unsuported_nested(object)
+
+      const value = `[${ object
+          .map(Serializer.value)
+      // the serializer would return an already formatted key
+      // since we are in a array we extract the value the ugly way
+      // this allows to keep the whole design simple elsewhere
+          .map(({ keys }) => keys[0].split('=')[1])
+          .join(',') }]`
+
+      return {
+        keys: [`${ prefix }=${ value }`],
+        raw : `$${ prefix }`,
+      }
+    }
 
     // a value may be a custom input like the += operator which
     // is manually supported in this lib and not yet in redisgraph
     // so we return the operator output
-    if (OPERATOR in object) return object[OPERATOR]
+    if (OPERATOR in object) return object[OPERATOR](prefix)
 
-    const entries = Object.entries(object)
-        .map(([key, value]) => `${ key }:${ Serializer.value(value) }`)
+    const prefixed = key => `${ prefix }_${ key }`
+    const keys = Object.entries(object).map(([key, value]) => {
+      if (value && typeof value === 'object' && !Array.isArray(value))
+        throw unsuported_nested(value)
+
+      const { keys: serialized } = Serializer.value(value, prefixed(key))
+
+      return serialized
+    })
+    const raw = Object.entries(object)
+        .map(([key]) => `${ key }:$${ prefixed(key) }`)
         .join()
 
-    return `{${ entries }}`
+    return {
+      keys,
+      raw: `{${ raw }}`,
+    }
   },
 }
 
